f93ec6ee43a712f1b851bc05163c237a76b1042f
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2022-04-27 09:18:16 -0400

    ✨ update: js - for loop

--- JavaScript/loop/pitfall.js
+++ JavaScript/loop/pitfall.js
@@ -31,3 +31,28 @@ function findEvenNumber(arr) {
 
 console.log(findEvenNumber([1, 3, 5])); // Outputs: false
 console.log(findEvenNumber([1, 4, 5])); // Outputs: false, even though there's an even number!
+
+
+// for-in loop is designed for iterating over an objects properties
+var obj = {a:1,b:2};
+for (let prop in obj) {
+    console.log(prop);
+}
+// a
+// b
+
+let array00 = [10,20,30];
+for (let index in array00) {
+  console.log(index);
+};
+// 0
+// 1
+// 2
+
+let array01 = [10,20,30];
+for (let index in array01) {
+  console.log(typeof(index));
+};
+// string - NOT NUMBER
+// string - NOT NUMBER
+// string - NOT NUMBER
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # JS-Interview-Exercises
+
+### Great resources
+1. https://codecraft.tv/courses/angular/es6-typescript/mapset/
